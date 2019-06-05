bd81c8b430697fcb43fab6770314bc0aa55e17f2
Graham Moore <github@cute.org.uk>
2019-06-05 22:19:49 +0100

    #1 not sure what is going on....

--- change.py
+++ change.py
@@ -1,12 +1,86 @@
+#Task 2: GCSE style question
+#Write a program that will add up the cost of several items.
+
+#The program should ask the user to enter how many items there are,
+#and then ask for the price of each item with the prompt message in the form
+#“Please enter the price of the item: £”.
+
+#The user enters the price of each item as a number without a currency symbol.
+
+#The program should add the prices together and then display the total cost
+#with a user-friendly message, including the currency symbol, £.
+
+#Challenge: Develop your program further so that the user enters the amount
+#that they have paid (e.g. 20.00 without the £ symbol) and the program displays
+#how many £20, £10, £5 notes and how many £2, £1, 50p, 20p, 10p, 5p, 2p and 1p
+#coins should be given as change, or how much more money is needed.
+#For the above challenge, the // operator (floor division, or integer division)
+#and % modulo operator (gives the positive remainder from integer division)
+#are helpful.
+#e.g.
+#5 // 2 = 2             (the integer part of the division calculation)
+#5 % 2 = 1             (the remainder from integer division)
+
 from decimal import *
+# getcontext().prec = 10
 
-getcontext().prec = 2
 
-change = Decimal("0.1")
-print(" new change before 1p %s" % (change))
+#initialise cost
+cost = Decimal("0.00")
 
-if change > Decimal("0.01"):
-    print("The number of one pences is 1 ")
-    change = change - Decimal("0.01")
+#initialise total
+totalcost = Decimal("0.00")
 
-print(" new change at end %s" % (change))
+#function to calculate how many £20, £10, £5 notes and how many £2, £1, 50p, 20p, 10p, 5p, 2p and 1p
+#coins should be given as change
+def calculatechangeformat(change):
+
+    print ("Stating change is £%s" % (change))
+
+    change = subtract_coin_or_note(change, "£20", "20")
+    change = subtract_coin_or_note(change, "£10", "20")
+    change = subtract_coin_or_note(change, "£5", "20")
+    change = subtract_coin_or_note(change, "£2", "20")
+    change = subtract_coin_or_note(change, "£1", "20")
+    change = subtract_coin_or_note(change, "50p", "20")
+    change = subtract_coin_or_note(change, "20p", "20")
+    change = subtract_coin_or_note(change, "10p", "20")
+    change = subtract_coin_or_note(change, "5p", "20")
+    change = subtract_coin_or_note(change, "2p", "20")
+    change = subtract_coin_or_note(change, "1p", "20")
+
+    print(" new change at end %s" % (change))
+
+
+def subtract_coin_or_note(change, faceValue, noteOrCoinValue):
+    print(" new change before %s is %s" % (faceValue, change))
+
+    note = Decimal(noteOrCoinValue)
+    if change >= note:
+        print ("The number of %s notes needed is 1" % faceValue)
+        change = Decimal(change) - note
+
+    return change
+
+
+
+#Ask the user to enter how many items there are
+#numofitems = int(input("How many items are there? "))
+
+#for each item get the price
+#for count in range(0,numofitems):
+#   cost = float(input("Please enter the price of the item: £"))
+#  totalcost = totalcost + cost
+# print ("Total so far £{0:.2f}".format(totalcost))
+
+#get amount paid from user
+#paid = float(input("How much have you paid so far? £"))
+#if paid > totalcost:
+#    change = paid-totalcost
+#    print("Your change is £{0:.2f}".format(change))
+calculatechangeformat(Decimal("38.86"))
+#elif paid < totalcost:
+#   owed = totalcost-paid
+#  print("You still owe £{0:.2f}".format(owed))
+#else :
+#   print("Thankyou thats perfect - Enjoy your day")
